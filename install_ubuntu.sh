c87a1e1d06505c93a76d6d01a23a366f0fa90dbc
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2022-12-24 13:34:55 -0300

    a

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -15,7 +15,7 @@ echo "Encoding=UTF-8" >> /usr/share/applications/openbox_config_menu.desktop
 echo "Version=1.0" >> /usr/share/applications/openbox_config_menu.desktop
 echo "Type=Application" >> /usr/share/applications/openbox_config_menu.desktop
 echo "Terminal=true" >> /usr/share/applications/openbox_config_menu.desktop
-echo "Exec=python3 /usr/share/openbox_config_menu/config_menu/menu.py" >> /usr/share/applications/openbox_config_menu.desktop
+echo "Exec=python3 /usr/share/openbox_config_menu/menu.py" >> /usr/share/applications/openbox_config_menu.desktop
 echo "Name=openbox config menu" >> /usr/share/applications/openbox_config_menu.desktop
 echo "Icon=/usr/share/openbox_config_menu/config_menu/config_icon.png" >> /usr/share/applications/openbox_config_menu.desktop
 
