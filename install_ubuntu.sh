f4ccca2479d61294ee2ba46a4ba91705939ac040
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2022-12-23 23:58:45 -0300

    a

---
+++ install_ubuntu.sh
@@ -0,0 +1,15 @@
+sudo apt -y install openbox plank rofi tint2 xcompmgr 
+sudo apt -y install firefox thunar gnome-icon-theme-full gnome-terminal
+
+sudo cp -R /config_menu /usr/share
+
+sudo touch  /usr/share/applications/openbox_config_menu.desktop
+cat <<EOT >> /usr/share/applications/openbox_config_menu.desktop
+Encoding=UTF-8
+Version=1.0
+Type=Application
+Terminal=true
+Exec=python3 /usr/share/openbox_config_menu/config_menu/menu.py
+Name=openbox config menu
+Icon=/usr/share/openbox_config_menu/config_menu/config_icon.png
+EOT
