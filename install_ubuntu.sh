ad3ca2bb0101b3e88fab0a333a2f173dbe27038e
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2022-12-24 13:22:49 -0300

    a

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -9,12 +9,12 @@ sudo cp -R ./config_menu /usr/share/openbox_config_menu
 
 sudo rm /usr/share/applications/openbox_config_menu.desktop
 sudo touch  /usr/share/applications/openbox_config_menu.desktop
-sudo cat <<EOT >> /usr/share/applications/openbox_config_menu.desktop
-Encoding=UTF-8
-Version=1.0
-Type=Application
-Terminal=false
-Exec=python3 /usr/share/openbox_config_menu/config_menu/menu.py
-Name=openbox config menu
-Icon=/usr/share/openbox_config_menu/config_menu/config_icon.png
-EOT
+
+echo "Encoding=UTF-8" >> /usr/share/applications/openbox_config_menu.desktop
+echo "Version=1.0" >> /usr/share/applications/openbox_config_menu.desktop
+echo "Type=Application" >> /usr/share/applications/openbox_config_menu.desktop
+echo "Terminal=false" >> /usr/share/applications/openbox_config_menu.desktop
+echo "Exec=python3 /usr/share/openbox_config_menu/config_menu/menu.py" >> /usr/share/applications/openbox_config_menu.desktop
+echo "Name=openbox config menu" >> /usr/share/applications/openbox_config_menu.desktop
+echo "Icon=/usr/share/openbox_config_menu/config_menu/config_icon.png" >> /usr/share/applications/openbox_config_menu.desktop
+
