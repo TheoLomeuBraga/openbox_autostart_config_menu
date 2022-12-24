2045080fd1ba8e5d848e8cff82d59bfb404e908f
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2022-12-24 00:03:32 -0300

    a

---
+++ config_menu/menu.py
@@ -0,0 +1,10 @@
+class openbox_config:
+    def __init__(self):
+        self.walpaper = True
+        self.window_compositor = True
+        self.taskbar = True
+        self.dock = True
+        self.network_applet = True
+        self.audio_applet = True
+        self.bluetooth_applet = False
+
