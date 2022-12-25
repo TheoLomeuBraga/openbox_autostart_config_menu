62ef41a482a755204f0178c94563ef5c0f97b7fc
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2022-12-25 10:22:19 -0300

    A

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -22,3 +22,18 @@ echo "Type=Application" >> /usr/share/applications/openbox_config_menu.desktop
 echo "Name=openbox config menu" >> /usr/share/applications/openbox_config_menu.desktop
 echo "Icon=/usr/share/openbox_config_menu/config_menu/config_icon.ico" >> /usr/share/applications/openbox_config_menu.desktop
 
+
+
+
+rm -r ~/.config/nitrogen
+rm -r ~/.config/openbox
+rm -r ~/.config/plank
+rm -r ~/.config/rofi
+rm -r ~/.config/tint2
+
+
+cp -R ./config/nitrogen ~/.config/nitrogen
+cp -R ./config/openbox ~/.config/openbox
+cp -R ./config/plank ~/.config/plank
+cp -R ./config/rofi ~/.config/rofi
+cp -R ./config/tint2 ~/.config/tint2
